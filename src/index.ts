5a6aeca25fe0aa532e7921577d00bdae7b1152fb
euijinkk <gytks4@naver.com>
2025-03-19 22:43:16 +0900

    chore: openai 세팅

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,40 @@
 import { Hono } from "hono";
+import OpenAI from "openai";
 
-const app = new Hono();
+interface Env {
+  OPENAI_API_KEY: string;
+}
 
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
-});
+export default {
+  async fetch(
+    request: Request,
+    env: Env,
+    ctx: ExecutionContext
+  ): Promise<Response> {
+    const app = new Hono<{ Bindings: Env }>();
 
-export default app;
+    const client: OpenAI = new OpenAI({
+      apiKey: env.OPENAI_API_KEY,
+    });
+
+    app.get("/", (c) => {
+      return c.text("Hello Hono!");
+    });
+
+    app.get("/test", async (c) => {
+      const completion = await client.chat.completions.create({
+        model: "gpt-4o",
+        messages: [
+          {
+            role: "user",
+            content: "Write a one-sentence bedtime story about a unicorn.",
+          },
+        ],
+      });
+
+      return c.body(completion.choices[0].message.content ?? "");
+    });
+
+    return app.fetch(request, env, ctx);
+  },
+};
