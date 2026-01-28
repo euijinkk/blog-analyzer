01dde5b607e85a4a8aebca5f7868b07274fc26c3
euijinkk <gytks4@naver.com>
2026-01-28 23:32:54 +0900

    feat: 블로그 글자수 제한하여 token 수 개선

--- src/blog-fetcher/parse-blog.ts
+++ src/blog-fetcher/parse-blog.ts
@@ -1,19 +1,21 @@
 import { RSSPostType } from "./type";
 
+const MAX_DESCRIPTION_LENGTH = 1000;
+
 export const parseBlogIntoString = ({
   blogPosts,
 }: {
   blogPosts: RSSPostType[];
 }) => `
 ${blogPosts
-  .map(
-    (blog, index) => `
+    .map(
+      (blog, index) => `
 ### 글 ${index + 1}
 - 제목: ${blog.title}
 - 작성자: ${blog.author}
-- 설명: ${blog.description}
+- 설명: ${blog.description.slice(0, MAX_DESCRIPTION_LENGTH)}
 - 링크: ${blog.link}
 `
-  )
-  .join("\n")}
+    )
+    .join("\n")}
 `;
