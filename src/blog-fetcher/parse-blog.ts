1c1adce8a6990f09538ad2db3f3236168ae18865
euijinkk <gytks4@naver.com>
2026-01-30 00:04:36 +0900

    feat: 불필요한 작성자 태그 제거하여 LLM 호출 비용 절약

--- src/blog-fetcher/parse-blog.ts
+++ src/blog-fetcher/parse-blog.ts
@@ -7,12 +7,13 @@ export const parseBlogIntoString = ({
 }: {
   blogPosts: RSSPostType[];
 }) => `
+작성자: ${blogPosts[0].author}
+
 ${blogPosts
     .map(
       (blog, index) => `
 ### 글 ${index + 1}
 - 제목: ${blog.title}
-- 작성자: ${blog.author}
 - 설명: ${blog.description.slice(0, MAX_DESCRIPTION_LENGTH)}
 - 링크: ${blog.link}
 `
