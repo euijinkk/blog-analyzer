a7e27d67f2ab714f1723ad2804470b75bdea37e2
euijinkk <gytks4@naver.com>
2026-02-11 23:26:42 +0900

    feat: blog_title NOT NULL

--- migrations/0002_add_blog_title_to_blog_report_v2.sql
+++ migrations/0002_add_blog_title_to_blog_report_v2.sql
@@ -1,3 +1,4 @@
-ALTER TABLE blog_report_v2 ADD COLUMN blog_title TEXT;
+ALTER TABLE blog_report_v2 
+ADD COLUMN blog_title TEXT NOT NULL DEFAULT '';  
 
 CREATE INDEX idx_blog_report_v2_blog_title ON blog_report_v2(blog_title);
